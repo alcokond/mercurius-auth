e608db56d4a6389573b6368f72b1ea23199e0d51
Alan Coello <alancoello.12@gmail.com>
2022-10-17 17:26:38 -0500

    primer commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    <div className="logo-xtrim" />
+    <p>
+      Powered by <a href="https://www.xtrim.com.ec/">Xtrim TvCable</a>
+    </p>
+  </footer>
+);
+
+export default Footer;

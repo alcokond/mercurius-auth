a6ca42c55257c5e6a58382862b8f0fe5dbfe00ce
Alan Coello <alancoello.12@gmail.com>
2022-11-23 17:22:14 -0500

    button square

--- src/views/Componentes.js
+++ src/views/Componentes.js
@@ -11,7 +11,7 @@ import PermanentDrawerLeft from "../components/PermanentDrawerLeft.js";
 import { CopyBlock, dracula } from "react-code-blocks";
 import TopBar  from "../components/TopBar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faPuzzlePiece } from "@fortawesome/pro-light-svg-icons";
+import { faArrowLeft, faCode, faPuzzlePiece } from "@fortawesome/pro-light-svg-icons";
 
 
 export const Componentes = () => {
@@ -938,136 +938,42 @@ export const Componentes = () => {
     </div> }
     
     </div>
+    <div className=" mb-5">
+    <h5 className=' text-heading-5 mb-3'>Boton Pink Secundario</h5>
     
+    {isMostrarPinkSecondary1 && 
+    !isShownPinkSecondary && <> 
+    <div className="flex flex-col ">
+    <div style={{alignSelf: "flex-end"}}>
+    <button className=" text-color-primary text-weight-semibold" onClick={handleClickPinkSecondary}>Ver Código <FontAwesomeIcon icon={faCode} /></button>
+    </div>
+    <div className="component-content-full flex">
+        <div className="mx-auto my-auto">
+          <button className="button-square "><FontAwesomeIcon icon={faArrowLeft } /></button>            
+        </div>
+    </div>
+    </div>
     
-      {/* <div id="cards">
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-apartment"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-apartment"></i>
-          <div class="card-info-title">
-            <h3>Botón primario mediano</h3>  
-            <TextareaAutosize className="textarea-code" readOnly value="<button class='button-main-primary-medium' >Button</button>">
-            </TextareaAutosize>
-          </div>    
+     </>  }
+     {isMostrarPinkSecondary1 && 
+    isShownPinkSecondary &&
+    <div className="flex flex-col ">
+    <div style={{alignSelf: "flex-end"}}>
+    <button className=" text-color-primary text-weight-semibold" onClick={handleClickPinkSecondary}>Ver Componente <FontAwesomeIcon icon={faPuzzlePiece} /></button> 
+    </div>
+    <div style={{minHeight: "25vh", backgroundColor:"rgb(40, 42, 54)", borderRadius:"8px" }}>
+     <CopyBlock 
+          language="html"
+          text={`<button class="button-square "></button>`}
+          codeBlock
+          theme={dracula}
+          showLineNumbers={false}
+        
+        />
         </div>
-      </div>
+    </div> } 
     </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-unicorn"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-unicorn"></i>
-          <div class="card-info-title">
-            <h3>Unicorns</h3>  
-            <h4>A single corn. Er, I mean horn.</h4>
-          </div>    
-        </div>  
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-blender-phone"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-blender-phone"></i>
-          <div class="card-info-title">
-            <h3>Blender Phones</h3>  
-            <h4>These absolutely deserve to exist.</h4>
-          </div>    
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-person-to-portal"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-person-to-portal"></i>
-          <div class="card-info-title">
-            <h3>Adios</h3>  
-            <h4>See you...</h4>
-          </div>    
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-person-from-portal"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-person-from-portal"></i>
-          <div class="card-info-title">
-            <h3>I mean hello</h3>  
-            <h4>...over here.</h4>
-          </div>    
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="card-image">
-        <i class="fa-duotone fa-otter"></i>
-      </div>
-      <div class="card-info-wrapper">
-        <div class="card-info">
-          <i class="fa-duotone fa-otter"></i>
-          <div class="card-info-title">
-            <h3>Otters</h3>  
-            <h4>Look at me, imma cute lil fella.</h4>
-          </div>    
-        </div>
-      </div>
-    </div>
-  </div>
-</div> */}
-</div>
-
-
-
-{/* 
-      <div className="flex flex-wrap gap-20">
-      <div>
-      <Label>Botón primario mediano</Label>
-      <MercButton1/>
-      <TextareaAutosize className="textarea-code" readOnly value="<button class='button-main-primary-medium' >Button</button>">
-      
-      </TextareaAutosize>
-      </div>
-      <div>
-      <Label>Botón secundario mediano</Label>
-      <MercButton2/>
-      <TextareaAutosize className="textarea-code" readOnly value="<button class='button-main-secondary-medium' >Button</button>">
-      
-      </TextareaAutosize>
-      </div>
-      <div>
-      <Label>Botón sutil mediano</Label>
-      <MercButton3/>
-      <TextareaAutosize className="textarea-code" readOnly value="<button class='button-main-subtle-medium' >Button</button>">
-      
-      </TextareaAutosize>
-      </div>
-      </div> */}
-      
+</div>    
     </>
   );
 };

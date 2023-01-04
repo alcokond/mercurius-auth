4f565059405221f724c9e85268777a8931f35cfa
Alan Coello <50714604+alcokond@users.noreply.github.com>
2023-01-04 17:26:02 -0500

    parallax

--- src/components/HeroNew.js
+++ src/components/HeroNew.js
@@ -4,10 +4,65 @@ import {faIcons, faPalette, faPenNib, faTypewriter, faArrowRight, facircleuser, 
 import { RadioGroup, FormControl, Radio, FormGroup, FormControlLabel, Checkbox, FormLabel } from "@mui/material";
 import {  NavLink } from "react-router-dom";
 import { NavLink as RouterNavLink } from "react-router-dom";
-
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 const Hero = () => (
   <>
-    <div className="">
+    <div>
+    <Parallax pages={4.5} >
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={1}
+          style={{
+            backgroundImage: `url("https://www.xtrim.com.ec/wp-content/uploads/2023/01/cielonocturno.jpg")`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.2}
+          factor={1}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            
+          }}
+        >
+
+          <img src='https://www.xtrim.com.ec/wp-content/uploads/2023/01/Planeta.png'
+          style={{ height: '100%' }}>
+
+          </img>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.3}
+          factor={1}
+          style={{
+            marginTop: '30%',
+            
+          }}
+        >
+          <img src='https://www.xtrim.com.ec/wp-content/uploads/2023/01/terreno1.png'
+          style={{width: '100%'}}></img>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          factor={2}
+          
+        >
+          <div className="">
       <section style={{backgroundColor: "#F2F1FC"}} className="header relative pt-16 items-center flex pb-16">
         <div  className=" mx-10 items-center flex flex-wrap">
           <div className="w-full md:w-8/12 lg:w-6/12-2 xl:w-6/12-2">
@@ -305,6 +360,30 @@ const Hero = () => (
         </div>
       </section>
       </div>
+        </ParallaxLayer>
+
+        {/* <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src= 'https://www.xtrim.com.ec/wp-content/uploads/2023/01/Cohete-volteado-gif.gif' />
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        ></ParallaxLayer> */}
+      </Parallax>
+      </div>
+    
       
       
     </>
---
+++ src/views/Intro.jsx
@@ -0,0 +1,85 @@
+import { useRef } from 'react';
+
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function Intro() {
+  const ref = useRef();
+
+  return (
+    <div>
+    <Parallax pages={3} >
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={1}
+          style={{
+            backgroundImage: `url("https://www.xtrim.com.ec/wp-content/uploads/2023/01/cielonocturno.jpg")`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={1}
+          style={{
+            marginTop: '4rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            
+          }}
+        >
+
+          <img src='https://www.xtrim.com.ec/wp-content/uploads/2023/01/Planeta.png'
+          style={{ height: '100%' }}>
+
+          </img>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0}
+          speed={2}
+          factor={1}
+          style={{
+            marginTop: '30%',
+            
+          }}
+        >
+          <img src='https://www.xtrim.com.ec/wp-content/uploads/2023/01/terreno1.png'
+          style={{width: '100%'}}></img>
+        </ParallaxLayer>
+
+        {/* <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src= 'https://www.xtrim.com.ec/wp-content/uploads/2023/01/Cohete-volteado-gif.gif' />
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        ></ParallaxLayer> */}
+      </Parallax>
+      </div>
+  
+);
+        }
+export default Intro;
